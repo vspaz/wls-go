a69689021501c85e190687ffa644811b39664b2c
vspaz <vladimir.edelman@gmail.com>
2022-02-20 15:08:14 +0100

    add getters

--- pkg/models/point.go
+++ pkg/models/point.go
@@ -4,3 +4,18 @@ type Point struct {
 	intercept float64
 	slope     float64
 }
+
+func NewPoint(intercept, slope float64) Point {
+	return Point{
+		intercept: intercept,
+		slope:     slope,
+	}
+}
+
+func (p *Point) GetIntercept() float64 {
+	return p.intercept
+}
+
+func (p *Point) GetSlope() float64 {
+	return p.slope
+}
