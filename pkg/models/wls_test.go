caec0b54c85118083a5b65cf37fd05ca835e24bc
vspaz <vladimir.edelman@gmail.com>
2022-02-20 17:25:15 +0100

    add TestWlsModelWithStableWeights

--- pkg/models/wls_test.go
+++ pkg/models/wls_test.go
@@ -1,7 +1,15 @@
 package models
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestWlsModelWithStableWeights(t *testing.T) {
-
+	x := []float64{1, 2, 3, 4, 5, 6, 7}
+	y := []float64{1, 2, 3, 4, 5, 6, 7}
+	wls := NewWlsWithoutWeights(x, y)
+	point := wls.FitLinearRegression()
+	assert.InDelta(t, 2.14285714, point.GetIntercept(), 6)
+	assert.InDelta(t, 0.25, point.GetSlope(), 6)
 }
