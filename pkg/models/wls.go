1f231d7c217d4a3b241b1996a361a7c7f6fe1fb8
vspaz <vladimir.edelman@gmail.com>
2022-02-20 15:45:38 +0100

    add constructors

--- pkg/models/wls.go
+++ pkg/models/wls.go
@@ -5,3 +5,28 @@ type Wls struct {
 	x []float64
 	w []float64
 }
+
+func populateWeights(capacity int, value float64) []float64 {
+	weights := make([]float64, capacity, capacity)
+	for idx, _ := range weights {
+		weights[idx] = value
+	}
+	return weights
+}
+
+func NewWlsWithoutWeights(x []float64, y []float64) Wls {
+	if len(x) != len(y) {
+		panic("the count of values doesn't match")
+	}
+	if len(x) < 2 {
+		panic("not enough points to fit linear regression")
+	}
+	return Wls{x: x, y: y, w: populateWeights(len(x), 1.0)}
+}
+
+func NewWlsWithWeights(x []float64, y []float64, w []float64) Wls {
+	if len(x) != len(y) {
+		panic("the count of values doesn't match")
+	}
+	return Wls{x: x, y: y, w: w[0 : len(x)-1]}
+}
