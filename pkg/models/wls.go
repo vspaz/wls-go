a5b8babfd3e1149c5bb291aca8fb34073b9b7abf
vspaz <vladimir.edelman@gmail.com>
2022-02-20 17:06:55 +0100

    'FitLinearRegression'

--- pkg/models/wls.go
+++ pkg/models/wls.go
@@ -30,3 +30,47 @@ func NewWlsWithWeights(x []float64, y []float64, w []float64) Wls {
 	}
 	return Wls{x: x, y: y, w: w}
 }
+
+func NewWlsWithEqualWeights(x []float64, y []float64, w float64) Wls {
+	if len(x) != len(y) {
+		panic("the count of values doesn't match")
+	}
+	return Wls{x: x, y: y, w: populateWeights(len(x), w)}
+}
+
+func (wls *Wls) FitLinearRegression() *Point {
+	sumOfWeights := 0.0
+	sumOfWeightsByXSquared := 0.0
+	sumOfXByYByWeights := 0.0
+	sumOfXByWeights := 0.0
+	sumOfYByWeights := 0.0
+
+	var xi, yi, wi, xiByWi float64
+	for i := 0; i < len(wls.x); i++ {
+		xi = wls.x[i]
+		yi = wls.y[i]
+		wi = wls.w[i]
+
+		sumOfWeights += wi
+		xiByWi = xi * wi
+		sumOfXByWeights += xiByWi
+		sumOfXByYByWeights += xiByWi * yi
+		sumOfYByWeights += yi * wi
+		sumOfWeightsByXSquared += xiByWi * xi
+	}
+
+	dividend := sumOfWeights*sumOfXByYByWeights - sumOfXByWeights*sumOfYByWeights
+	divisor := sumOfWeights*sumOfWeightsByXSquared - sumOfXByWeights*sumOfXByWeights
+
+	if divisor == 0 {
+		return nil
+	}
+
+	slope := dividend / divisor
+	intercept := (sumOfYByWeights - slope*sumOfXByWeights) / sumOfWeights
+
+	return &Point{
+		intercept: intercept,
+		slope:     slope,
+	}
+}
