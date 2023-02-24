f2437152125fd0504f77ae636a3ab9345a98abbc
vspaz <vladimir.edelman@gmail.com>
2023-02-25 00:12:04 +0100

    rename vars

--- examples/main.go
+++ examples/main.go
@@ -6,10 +6,10 @@ import (
 )
 
 func main() {
-	x := []float64{1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0}
-	y := []float64{1.0, 3.0, 4.0, 5.0, 2.0, 3.0, 4.0}
-	w := []float64{10.0, 1.0, 3.0, 8.0, 14.0, 21.0, 13.0}
-	wls := models.NewWlsWithWeights(x, y, w)
+	xPoints := []float64{1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0}
+	yPoints := []float64{1.0, 3.0, 4.0, 5.0, 2.0, 3.0, 4.0}
+	weights := []float64{10.0, 1.0, 3.0, 8.0, 14.0, 21.0, 13.0}
+	wls := models.NewWlsWithWeights(xPoints, yPoints, weights)
 	point := wls.FitLinearRegression()
 	fmt.Println(point.GetSlope())
 	fmt.Println(point.GetIntercept())
