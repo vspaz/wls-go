e5b013e35ec169d708dd0a634463dadc1e38b42b
vspaz <vladimir.edelman@gmail.com>
2023-02-25 00:10:52 +0100

    fix example

--- examples/main.go
+++ examples/main.go
@@ -6,9 +6,9 @@ import (
 )
 
 func main() {
-	x := []float64{1, 2, 3, 4, 5, 6, 7}
-	y := []float64{1, 2, 3, 4, 5, 6, 7}
-	w := []float64{1, 2, 3, 4, 5, 6, 7}
+	x := []float64{1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0}
+	y := []float64{1.0, 3.0, 4.0, 5.0, 2.0, 3.0, 4.0}
+	w := []float64{10.0, 1.0, 3.0, 8.0, 14.0, 21.0, 13.0}
 	wls := models.NewWlsWithWeights(x, y, w)
 	point := wls.FitLinearRegression()
 	fmt.Println(point.GetSlope())
