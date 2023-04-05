ce501ccdf2d87069e78ca0c9e43eeade2ccbff93
vspaz <vladimir.edelman@gmail.com>
2023-04-05 20:46:52 +0200

    add tests

--- pkg/models/wls_test.go
+++ pkg/models/wls_test.go
@@ -7,7 +7,7 @@ import (
 
 const delta = 1.0e-6
 
-func TestWlsModelWithStableWeights(t *testing.T) {
+func TestWlsModelWithStableWeightsOk(t *testing.T) {
 	x := []float64{1, 2, 3, 4, 5, 6, 7}
 	y := []float64{1, 3, 4, 5, 2, 3, 4}
 	wls := NewWlsWithoutWeights(x, y)
@@ -16,7 +16,16 @@ func TestWlsModelWithStableWeights(t *testing.T) {
 	assert.InDelta(t, 0.25, point.GetSlope(), delta)
 }
 
-func TestWlsModelWithSingleWeight(t *testing.T) {
+func TestWlsModelWithStableWeightsWidthDifferentDataTypesOk(t *testing.T) {
+	x := []int{1, 2, 3, 4, 5, 6, 7}
+	y := []float64{1, 3, 4, 5, 2, 3, 4}
+	wls := NewWlsWithoutWeights(x, y)
+	point := wls.FitLinearRegression()
+	assert.InDelta(t, 2.14285714, point.GetIntercept(), delta)
+	assert.InDelta(t, 0.25, point.GetSlope(), delta)
+}
+
+func TestWlsModelWithSingleWeightOk(t *testing.T) {
 	x := []float64{1, 2, 3, 4, 5, 6, 7}
 	y := []float64{1, 3, 4, 5, 2, 3, 4}
 	wls := NewWlsWithStableWeights(x, y, 0.9)
@@ -25,9 +34,19 @@ func TestWlsModelWithSingleWeight(t *testing.T) {
 	assert.InDelta(t, 0.25, point.GetSlope(), delta)
 }
 
-func TestWlsModelWithWeights(t *testing.T) {
+func TestWlsModelWithWeightsOk(t *testing.T) {
 	x := []float64{1, 2, 3, 4, 5, 6, 7}
 	y := []float64{1, 3, 4, 5, 2, 3, 4}
+	w := []float64{10, 0, 3, 4, 5, 7, 7}
+	wls := NewWlsWithWeights(x, y, w)
+	point := wls.FitLinearRegression()
+	assert.InDelta(t, 1.1433012, point.GetIntercept(), delta)
+	assert.InDelta(t, 0.3962990, point.GetSlope(), delta)
+}
+
+func TestWlsModelWithWeightsWithDifferentDataTypesOk(t *testing.T) {
+	x := []int8{1, 2, 3, 4, 5, 6, 7}
+	y := []float32{1.0, 3.0, 4.0, 5.0, 2.0, 3.0, 4.0}
 	w := []float64{10, 0, 3, 4, 5, 7, 7}
 	wls := NewWlsWithWeights(x, y, w)
 	point := wls.FitLinearRegression()
@@ -65,14 +84,14 @@ func TestRunDownhillOk(t *testing.T) {
 	assert.Equal(t, -1.0, point.GetSlope())
 }
 
-func TestPanicIfSequenceHasSingleValue(t *testing.T) {
+func TestSequenceHasSingleValueFail(t *testing.T) {
 	x := []float64{1}
 	y := []float64{1}
 
 	assert.Panics(t, func() { NewWlsWithoutWeights(x, y) })
 }
 
-func TestPanicIfSequencesOfDifferentSize(t *testing.T) {
+func TestSequencesOfDifferentSizeFail(t *testing.T) {
 	x := []float64{1, 2}
 	y := []float64{1, 2, 3}
 
